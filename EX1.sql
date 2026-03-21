412af5d2262a5be52a4c7fcffde020b4760daeff
hnimkwazeo <vunhatminh.vnw.03@gmail.com>
2026-03-21 15:02:42 +0700

    update

--- EX1.sql
+++ EX1.sql
@@ -11,18 +11,18 @@ insert into products(product_name, category) values
 ('Ghế xoay', 'Furniture');
 
 create table orders(
-	order_id serial primary key,
+	order_id int primary key,
 	product_id int references products(product_id),
 	quantity int not null,
 	total_price int not null
 );
 
-insert into orders(product_id, quantity, total_price) values
-(1,2,2200),
-(2,3,3300),
-(3,5,2500),
-(4,4,1600),
-(1,1,1100);
+insert into orders(order_id,product_id, quantity, total_price) values
+(101,1,2,2200),
+(102,2,3,3300),
+(103,3,5,2500),
+(104,4,4,1600),
+(105,1,1,1100);
 
 select sum(o.total_price) as "total_sales", sum(o.quantity) as "total_quantity", p.category as "category"
 from products p join orders o on p.product_id = o.product_id
--- EX2.sql
+++ EX2.sql
@@ -35,3 +35,4 @@ select p.category
 from products p join orders o on p.product_id = o.product_id
 group by p.category
 having sum(o.total_price) > 3000;
+
